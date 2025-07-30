463186c7274593d9fa6d0d074b1245f19f13fddd
Thiago Pedron <thiagopedron70@gmail.com>
2025-07-30 03:09:20 -0300

    arrumando countdouwn

--- js/pages/countdown.js
+++ js/pages/countdown.js
@@ -1,18 +1,9 @@
-import '../../css/pages/countdown.scss';
-
 function getParam(param, defaultValue) {
   const urlParams = new URLSearchParams(window.location.search);
   return urlParams.get(param) || defaultValue;
 }
 
 const targetDateStr = getParam("date", "2026-12-25");
-const eventTitle = decodeURIComponent(getParam("title", "My Event"));
-const bgColor = "#" + getParam("bg", "ffffff");
-const textColor = "#" + getParam("color", "000000");
-
-document.body.style.backgroundColor = bgColor;
-document.body.style.color = textColor;
-document.getElementById("eventTitle").textContent = eventTitle;
 
 const targetDate = new Date(targetDateStr + " 00:00:00").getTime();
 
